bf4284f113fe692940fda469de9e787910ab4aac
fergus-g <fergus.gildea@utrack.com>
2020-01-15 21:54:20 +0000

    Fixed issue with user association, working correctly now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,8 @@
 class ArticlesController <ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy, :show]
   def index
-
   @articles = Article.all
-
   end
-
 
   def new
         @article = Article.new
@@ -13,6 +10,7 @@ class ArticlesController <ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was saved!"
       redirect_to article_path(@article)
@@ -22,38 +20,24 @@ class ArticlesController <ApplicationController
 end
 
 def show
-
 end
 
 def edit
-
-
-
 end
 
 def update
-
-
-
-if @article.update(article_params)
-
-flash[:notice] = "Article was successfully updated"
-
-redirect_to article_path(@article)
-
-else
-
-render 'edit'
-
+  if @article.update(article_params)
+    flash[:notice] = "Article was successfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
 end
-
 end
 
 def destroy
-
   @article.destroy
-  flash[:notice] = "Article was successfully deleted"
-  redirect_to articles_path
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
 end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article <ActiveRecord::Base
-    #// Validator to make sure enteries are not nil
+    belongs_to :user
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 3, maximum: 500}
-
-
+    validates :user_id, presence: true
 end
