ddf5397557b6749ec40157ff6cb4aa6a98cee89d
fergus-g <59649706+fergus-g@users.noreply.github.com>
2020-02-03 14:08:22 +0000

    Design Updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,30 @@
-class UsersController <ApplicationController
+class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
   def create
-    debugger
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Riding Ruby on Rails #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
   end
+end
+
+  def edit
+      @user = User.find(params[:id])
+  end
+
+  def update
+
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
   end
